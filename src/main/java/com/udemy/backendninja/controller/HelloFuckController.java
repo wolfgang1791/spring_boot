3c6410ffa1ccaa95b09c2d166a60766d7e993ff8
Jonathan <jonathanperalta1893@gmail.com>
2018-08-31 15:31:06 -0500

    retornando desde controller a view

--- src/main/java/com/udemy/backendninja/controller/HelloFuckController.java
+++ src/main/java/com/udemy/backendninja/controller/HelloFuckController.java
@@ -1,15 +1,25 @@
 package com.udemy.backendninja.controller;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloFuckController {
 	
 	@GetMapping("/world")
-	public String helloFuck() {
+	public String helloFuck(Model model) {
+		model.addAttribute("name","Clemente");
 		return "helloworld";
+	}
+	
+	@GetMapping("/worldv")
+	public ModelAndView modelandview() {
+		ModelAndView mav = new	ModelAndView("helloworld");
+		mav.addObject("name","Celestino");
+		return mav;
 	}
 }
