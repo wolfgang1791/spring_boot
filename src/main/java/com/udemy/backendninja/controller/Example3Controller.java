93d566ab180240f5fd406b24895bbc55b13e77b0
Jonathan <jonathanperalta1893@gmail.com>
2018-08-31 20:37:21 -0500

    logs de metodos

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -1,5 +1,7 @@
 package com.udemy.backendninja.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,10 @@ import com.udemy.backendninja.model.Person;
 @Controller
 @RequestMapping("/example3")
 public class Example3Controller {
+	
+	private static final Log LOGGER = LogFactory.getLog(Example3Controller.class);
+	
+	
 	
 	public static final String view3 = "example3";
 	public static final String result_view = "result";
@@ -33,16 +39,20 @@ public class Example3Controller {
 	
 	@GetMapping("/showform")
 	public String showform(Model model) {
-		int a = 6/0;
-		System.out.println(a);
+		LOGGER.info("info trace");
+		LOGGER.warn("warning trace");
+		LOGGER.error("error trace");
+		LOGGER.debug("debug trace");
 		model.addAttribute("person", new Person());
 		return view3;
 	}
 	
 	@PostMapping("/addperson")
 	public ModelAndView addperson(@ModelAttribute("person") Person person) {
+		LOGGER.info("METHOD: 'addperson' -- PARAMS:'"+person+"'");
 		ModelAndView mav = new ModelAndView(result_view);
 		mav.addObject("person",person);
+		LOGGER.info("TEMPLATE: "+result_view+" -- DATA:'"+person+"'");
 		return mav;
 	}
 }
--- src/main/java/com/udemy/backendninja/model/Person.java
+++ src/main/java/com/udemy/backendninja/model/Person.java
@@ -24,4 +24,8 @@ public class Person {
 	}
 	
 	public Person() {}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
 }
