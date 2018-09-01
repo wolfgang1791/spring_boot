e601b7641e5342190247f7237b76591228a2cf90
Jonathan <jonathanperalta1893@gmail.com>
2018-08-31 20:03:28 -0500

    2 formas de redireccionamiento

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.backendninja.model.Person;
 
@@ -16,6 +17,19 @@ public class Example3Controller {
 	
 	public static final String view3 = "example3";
 	public static final String result_view = "result";
+	
+	//n1
+	/*
+	@GetMapping("/")
+	public String redirect() {
+		return "redirect:/example3/showform";
+	}*/
+	
+	//n2
+	@GetMapping("")
+	public RedirectView redirectView() {
+		return new RedirectView("/example3/showform");
+	}
 	
 	@GetMapping("/showform")
 	public String showform(Model model) {
